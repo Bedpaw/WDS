3b829a4866064fa35004c855d25e093bb03572b5
Paweł Bednarczyk <bedpaw97@gmail.com>
2020-10-08 19:01:49 +0200

    Add price list content

--- moja-aplikacja/src/components/PriceList/PriceList.js
+++ moja-aplikacja/src/components/PriceList/PriceList.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './PriceList.css';
+import {passes} from "../../data/price-list";
 
 
 class PriceList extends React.Component {
 
     render() {
         return (
-            <>Cennik</>
+            passes.map(pass => {
+                    return <>{{pass.name}}</>
+            })
         )
     }
 }
