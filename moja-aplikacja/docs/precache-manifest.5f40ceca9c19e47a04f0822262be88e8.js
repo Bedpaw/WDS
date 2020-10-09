f51084dd3366b17d0c2289637f79c08c4249e902
Paweł Bednarczyk <bedpaw97@gmail.com>
2020-10-09 14:59:37 +0200

    Rename build for gp

---
+++ moja-aplikacja/docs/precache-manifest.5f40ceca9c19e47a04f0822262be88e8.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b59006bc20020777405829b2632c66a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82dc035010eeba53bd83",
+    "url": "/static/css/main.b0911e0c.chunk.css"
+  },
+  {
+    "revision": "738b51cacf9daa714f4f",
+    "url": "/static/js/2.e3346e6b.chunk.js"
+  },
+  {
+    "revision": "569f33c8d1d96bc5b64a6ce10183922b",
+    "url": "/static/js/2.e3346e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82dc035010eeba53bd83",
+    "url": "/static/js/main.4dc7cd5b.chunk.js"
+  },
+  {
+    "revision": "64cfa34d82d46c3cefdd",
+    "url": "/static/js/runtime-main.a8975b96.js"
+  },
+  {
+    "revision": "5501f10a4439ed95591f9619ccb43f4b",
+    "url": "/static/media/aboutUsBackground.5501f10a.jpg"
+  },
+  {
+    "revision": "36d46fecfcb042b4c88fa040e183e430",
+    "url": "/static/media/aleksandraFoltman.36d46fec.jpg"
+  },
+  {
+    "revision": "63e208ef4a7b2ad388fc07b4bf33a743",
+    "url": "/static/media/andrzejMorawiec.63e208ef.jpg"
+  },
+  {
+    "revision": "3acd2fc71eb34c840df5439c0624af09",
+    "url": "/static/media/emiliaWitowska.3acd2fc7.jpg"
+  },
+  {
+    "revision": "470639577af67b291386d52ac4802a38",
+    "url": "/static/media/grzegorzWolf.47063957.jpg"
+  },
+  {
+    "revision": "188f52ba6f0f99abf5754658e69c8504",
+    "url": "/static/media/katarzynaWiecek.188f52ba.jpg"
+  },
+  {
+    "revision": "7242d50b7b963fdd85af4462172d6ea2",
+    "url": "/static/media/landingPageBackground.7242d50b.jpg"
+  },
+  {
+    "revision": "478eab06cb047f99bc6d49153cb13522",
+    "url": "/static/media/logo.478eab06.jpg"
+  },
+  {
+    "revision": "5d7687971c280fc32b61201dd5cbd731",
+    "url": "/static/media/mateuszPietrzak.5d768797.jpg"
+  },
+  {
+    "revision": "0a55c032e2fab50f8e543e3296ac2f1c",
+    "url": "/static/media/pawelWitulski.0a55c032.jpg"
+  },
+  {
+    "revision": "d624ba7755909164c561e4de5a3b3f70",
+    "url": "/static/media/piotrBurba.d624ba77.jpg"
+  },
+  {
+    "revision": "f9b33f98eec27083511742ee1bce59ab",
+    "url": "/static/media/reniaGoslawska.f9b33f98.jpg"
+  }
+]);
